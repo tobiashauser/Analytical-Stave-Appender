de4cb6077cf1dd348a4764b7c51f7e0cff410be2
Tobias Hauser <apps.hausertobias@gmail.com>
2024-02-11 10:11:46 +0100

    feat: version 0.1.1

---
+++ README.md
@@ -0,0 +1,100 @@
+# Analytical Stave Appender
+
+![](assets/zwerg.jpg)
+
+## How to install?
+
+Conveniently install the tool with `pip` and use it from your command line:
+
+```
+pip install add_staves
+```
+
+### Windows
+
+Make sure `python` is installed on your machine. This can be tested from the
+terminal with:
+
+```
+python --version
+```
+
+If it isn't installed yet, you will be prompted to install it from the store.
+After the installation, pip will output a warning if its installation directory
+is not available in the global PATH. 
+
+> [!Note] 
+> I don't have access to a Windows machine.. I hope it works in most cases.
+
+<details>
+    <summary>Einen Pfad zu PATH hinzufügen</summary>
+<ol>
+<li><p>Open the Start menu and search for &#8220;Edit the system environment variables&#8221;, or type &#8220;Environment Variables&#8221; into the search bar and select &#8220;Edit the system environment variables&#8221; from the results.</p></li>
+<li><p>In the System Properties window, click on the &#8220;Environment Variables&#8230;&#8221; button.</p></li>
+<li><p>In the Environment Variables window, under the &#8220;System variables&#8221; section, locate the variable named &#8220;Path&#8221; and select it. Then, click on the &#8220;Edit&#8230;&#8221; button.</p></li>
+<li><p>In the Edit Environment Variable window, click on the &#8220;New&#8221; button.</p></li>
+<li><p>Enter the path you want to add in the provided field. Make sure to type the directory containing the executable files you want to access globally.</p></li>
+<li><p>Click &#8220;OK&#8221; to close each of the open windows.</p></li>
+<li><p>Restart your shell, in order for the new path to be picked up.</p></li>
+</ol>
+</details>
+<details>
+    <summary>Einen Pfad zu PATH hinzufügen</summary>
+<ol>
+<li><p>Öffnen Sie das Startmenü und suchen Sie nach &#8220;Systemumgebungsvariablen bearbeiten&#8221; oder geben Sie &#8220;Umgebungsvariablen&#8221; in die Suchleiste ein und wählen Sie &#8220;Umgebungsvariablen für Ihr Konto bearbeiten&#8221; aus.</p></li>
+<li><p>Im Fenster &#8220;Umgebungsvariablen&#8221; unter dem Abschnitt &#8220;Systemvariablen&#8221; suchen Sie nach &#8220;Path&#8221; und klicken Sie darauf, um es zu markieren. Klicken Sie dann auf &#8220;Bearbeiten&#8230;&#8221;.</p></li>
+<li><p>Klicken Sie im Fenster &#8220;Systemvariablen bearbeiten&#8221; auf &#8220;Neu&#8221;.</p></li>
+<li><p>Geben Sie den Pfad ein, den Sie hinzufügen möchten, und klicken Sie auf &#8220;OK&#8221;.</p></li>
+<li><p>Klicken Sie auf &#8220;OK&#8221;, um das Fenster &#8220;Umgebungsvariablen&#8221; zu schließen, und dann erneut auf &#8220;OK&#8221;, um das Fenster &#8220;Systemeigenschaften&#8221; zu schließen.</p></li>
+<li><p>Starten Sie die Eingabeaufforderung neu, damit der Pfad aktualisiert wird.</p></li>
+</ol>
+</details>
+
+### Briss
+
+Briss depends on a java runtime. When you open it you will be prompted to install it (if it's not already on your system).
+
+## Usage
+
+```
+add-staves path/to/your/score.pdf
+```
+
+Further information about its usage can be found in its help: `add-staves --help`.
+
+## Separate a score into systems
+
+You can use any program of your choice in order to seperate a score into systems.
+
+> [!Important] 
+> Make sure that each system or part you want to append analytical staves to,
+> is its own page in the PDF.
+
+The following tool works quite well, but any other suggestions are 
+much appreciated.
+
+### Briss
+
+[BRISS](https://briss.sourceforge.net/) is a cross-platform application for 
+cropping PDF-files. By default, it will try to find common areas on all the
+pages and overlay them in the interface, so that you only have to declare 
+the area to crop once and not on all pages. However, this doesn't work 
+particularly well for scores. This behaviour can be circumvented by passing 
+a range from the first to the last page (e.g. "1-4") to the dialog showing
+immediatly after loading a document.
+
+#### Installation
+
+On a mac, `briss` can be installed and started from the command line with:
+
+```
+brew install briss
+briss path/to/score.pdf
+```
+
+For usage on Windows, an executable can be downloaded [here](https://sourceforge.net/projects/briss/files/latest/download). After unzipping, double-clicking will
+launch Briss. 
+
+> [!Note]
+> Briss needs a java runtime to be installed on the system. You will prompted to 
+> install it, if it doesn't exist.
---
+++ add_staves/main.py
@@ -0,0 +1,418 @@
+import os
+import re
+from dataclasses import dataclass
+from functools import reduce
+from pathlib import Path
+from typing import Annotated, List, Optional, Tuple
+
+import typer
+from pypdf import PageObject, PaperSize, PdfReader, PdfWriter
+from rich import print
+
+# The CLI app
+app = typer.Typer(rich_markup_mode="rich")
+
+
+@dataclass
+class PageLayout:
+    """
+    Convenience class that wraps the page configuration options
+    so that they can be easily passed around.
+    """
+
+    top_margin: int
+    bottom_margin: int
+    top_padding: int
+    bottom_padding: int
+    stave_height: float
+    minimum_height: float = PaperSize.A4.height
+
+    def calculate_stave_height(self) -> float:
+        return self.stave_height + self.bottom_padding + self.top_padding
+
+
+def output_callback(ctx: typer.Context, input: Optional[Path]) -> Path:
+    """
+    Callback for the output option in 'run' that creates a
+    default output path if none was provided:
+
+    path/to/score.pdf -> path/to/score-analysis.pdf
+
+    Creating the default path can fail in case a file
+    with this name already exists. Providing the '--force'-flag
+    allows overwriting of this file.
+    """
+    if input is None:
+        score = Path(ctx.params["score"])
+        path = score.parent / (score.stem + "-analysis.pdf")
+        if path.exists() and not ctx.params["force"]:
+            print(
+                "[red]A file at the default path [bright_white]'%s'[/] already exists. Please provide an explicit output path with the [green bold]'-o'[/] option or force overwriting with the [green bold]'-f'[/] flag.[/]"
+                % path
+            )
+            raise typer.Exit(1)
+        return path
+    else:
+        return input
+
+
+def groups_parser(input: Optional[str]) -> Optional[List[int]]:
+    """
+    Parse a string of comma-separated or space-separated
+    numbers into a list of integers.
+
+    This will default to an empty list if an error occurs.
+    """
+    if input is None:
+        return None
+
+    try:
+        numbers = [int(num) for num in input.split(",")]
+    except:
+        numbers = []
+
+    try:
+        numbers = [int(num) for num in re.split(r"\s+", input)]
+    except:
+        pass
+
+    return numbers if all(num > 0 for num in numbers) else []
+
+
+# def groups_callback(ctx: typer.Context, input: Optional[List[int]]) -> Optional[List[int]]:
+def groups_callback(score: PdfReader, groups: Optional[List[int]]) -> Optional[List[int]]:
+    """
+    🚨 No access to the score argument in the params.
+
+    If no combining flag (input is None) is given, the tool tries to fit as
+    many systems as possible on a DIN A4 page.
+
+    If, however the option is used – be it empty or incomplete –
+    the tool disregards the DIN A4 size constraint and respects
+    as much input as possible:
+
+    1. The user provides an empty string: Everything will end up
+       on one page.
+
+    2. The sum of the groupings is less than the total page count:
+       The remaining pages will be output on one page.
+
+    3. The sum of the grouping is more than the total page count:
+       The overflow will be discarded.
+    """
+    if groups is None:
+        return None
+
+    # number of pages in total
+    page_count = len(score.pages)
+
+    # sum of total grouped pages
+    grouped_pages_count = sum(groups)
+
+    if sum(groups) == 0:
+        return [page_count]
+    elif grouped_pages_count < page_count:
+        return groups + [page_count - grouped_pages_count]
+    elif grouped_pages_count > page_count:
+        while sum(groups) > page_count:
+            groups.pop()
+        else:
+            return groups + [page_count - sum(groups)]
+    else:
+        return groups
+
+
+def staves_parser(staves_count: int) -> PageObject:
+    """
+    Fetches the correct empty staves pdf from the resources.
+    """
+    staves_path = os.path.join(
+        os.path.dirname(__file__), "resources", "empty-%d.pdf" % int(staves_count)
+    )
+    return PdfReader(staves_path).pages[0]
+
+
+def calculate_min_height(systems: List[PageObject], page_layout: PageLayout) -> float:
+    """
+    Calculate the minimum height necessary to fit all systems
+    plus staves and whitespace on one page.
+    """
+    return (
+        reduce(
+            lambda x, y: x + y, list(map(lambda system: system.cropbox.height, systems)), 0
+        )
+        + page_layout.top_margin
+        + page_layout.bottom_margin
+        + len(systems)
+        * (page_layout.stave_height + page_layout.top_padding + page_layout.bottom_padding)
+    )
+
+
+def layout_systems(
+    systems: List[PageObject], staves: PageObject, writer: PdfWriter, page_layout: PageLayout
+):
+    """
+    Layout the systems with the empty staves on a new blank
+    page in the writer.
+
+    Dynamically adjust the paddings if the total space needed
+    is less than a DIN A4 page.
+    """
+    # minimum height necessary to layout all elements
+    minimum_height = calculate_min_height(systems, page_layout)
+
+    # flag to dynamically layout the elements
+    dynamic_layout = True if minimum_height < PaperSize.A4.height else False
+
+    # property height that represents the current level
+    # to which the page is filled
+    # it counts down from minimum_height -> zero
+    height = (
+        minimum_height
+        if minimum_height > page_layout.minimum_height
+        else page_layout.minimum_height
+    )
+
+    # the page to which to "# print" the elements
+    current_page = writer.add_blank_page(PaperSize.A4.width, height)
+
+    # insert the top_margin
+    height -= page_layout.top_margin
+
+    # space that needs to be dynamically allocated
+    # this is only interesting if the flag dynamic_layout is set
+    dynamic_spacing = (
+        page_layout.minimum_height
+        - minimum_height
+        - page_layout.bottom_margin
+        - page_layout.top_margin
+        if dynamic_layout
+        else 0
+    )
+
+    # calculate the additional_bottom_padding
+    additional_bottom_padding = dynamic_spacing / (3 * len(systems))
+    if additional_bottom_padding > 30:
+        additional_bottom_padding = 30
+
+    # remove the sum of the additional_bottom_paddings from the dynamic_spacing
+    dynamic_spacing -= additional_bottom_padding * len(systems)
+
+    # calculate the additional_top_padding
+    additional_top_padding = dynamic_spacing / len(systems)
+
+    # iterate over the systems and add them to the current_page
+    for system in systems:
+        # add the system
+        current_page.merge_translated_page(system, 0, height - system.cropbox.top)
+        height -= system.cropbox.height
+
+        # add bottom_padding
+        # dynamic_layout ? 1/3 up to max 30
+        height -= page_layout.bottom_padding + additional_bottom_padding
+
+        # add staves
+        current_page.merge_translated_page(
+            staves,
+            (PaperSize.A4.width - staves.cropbox.width) / 2,
+            height - staves.cropbox.height,
+        )
+        height -= staves.cropbox.height
+
+        # add top_padding
+        # dynamic_layout ? increase 2/3 or rest
+        height -= page_layout.top_padding + additional_top_padding
+
+
+@app.command(rich_help_panel=True)
+def run(
+    # Arguments
+    score: Annotated[
+        Path,
+        typer.Argument(
+            metavar="SCORE",
+            help="Path to the cropped score.",
+            show_default=False,
+            exists=True,
+            readable=True,
+        ),
+    ],
+    # Options
+    force: Annotated[
+        bool,
+        typer.Option(
+            "--force",
+            "-f",
+            help="Overwrite the output destination if it exists.",
+            is_flag=True,
+        ),
+    ] = False,
+    # Configuration
+    groups: Annotated[
+        Optional[str],
+        typer.Option(
+            "--combining",
+            "-c",
+            metavar="TEXT",
+            rich_help_panel="Configuration",
+            help="Groups of systems combined on one page as space- or comma-seperated text.",
+            parser=groups_parser,
+            # callback=groups_callback,
+        ),
+    ] = None,
+    output: Annotated[
+        Optional[Path],
+        typer.Option(
+            "--output",
+            "-o",
+            metavar="PATH",
+            help="Path to the output file. [bright_black]\\[default: <score>-analysis.pdf][/]",
+            rich_help_panel="Configuration",
+            file_okay=True,
+            show_default=False,
+            callback=output_callback,
+        ),
+    ] = None,
+    staves: Annotated[
+        int,
+        typer.Option(
+            "--staves",
+            "-s",
+            min=1,
+            max=6,
+            clamp=True,
+            help="Number of staves to add.",
+            metavar="NUMBER",
+            rich_help_panel="Configuration",
+            parser=staves_parser,
+        ),
+    ] = 2,
+    # Page Layout
+    top_margin: Annotated[
+        int,
+        typer.Option(
+            rich_help_panel="Page Layout",
+            help="Configure the margin at the top of the page.",
+            min=0,
+            clamp=True,
+            metavar="NUMBER",
+        ),
+    ] = 30,
+    bottom_margin: Annotated[
+        int,
+        typer.Option(
+            rich_help_panel="Page Layout",
+            help="Configure the margin at the bottom of the page.",
+            min=0,
+            clamp=True,
+            metavar="NUMBER",
+        ),
+    ] = 30,
+    top_padding: Annotated[
+        int,
+        typer.Option(
+            rich_help_panel="Page Layout",
+            help="Configure the padding above a system.",
+            min=0,
+            clamp=True,
+            metavar="NUMBER",
+        ),
+    ] = 30,
+    bottom_padding: Annotated[
+        int,
+        typer.Option(
+            rich_help_panel="Page Layout",
+            help="Configure the padding beneath a system.",
+            min=0,
+            clamp=True,
+            metavar="NUMBER",
+        ),
+    ] = 10,
+):
+    """
+    Add analytical staves to a score.
+
+    ---------------------------------
+
+    The tool will treat each page of the provided score as one unit
+    under which it will add empty staves. Therefore use an external
+    tool (e.g. '[link=https://briss.sourceforge.net/]briss[/link]') to separate the score into the desired units.
+
+    By default, the tool tries to fit as many systems (plus their staves)
+    as possible on one DIN A4 page. This behaviour can be overridden, by
+    providing a comma- or space-separated list of numbers, declaring
+    how many pages to group together. This is especially useful, if you
+    want to keep the page numbers of the original score:
+
+    Given a 2-page score of which the first page consists of 4 systems
+    and the second of 5 system, the command to keep this grouping is:
+
+    [green bold]add-staves my-score.pdf --combining "4 5"[/]
+
+    Take note of the option [green]--combining "4 5"[/]. This tells the
+    tool to combine the first 4 and then the next 5 pages on a single page.
+    Note, that a page will never be smaller than a DIN A4 page.
+    """
+    # read the score pdf
+    score = PdfReader(score)
+
+    # clean the groups
+    groups = groups_callback(score, groups)
+
+    # create the PageLayout class
+    page_layout = PageLayout(
+        top_margin,
+        bottom_margin,
+        top_padding,
+        bottom_padding,
+        staves.cropbox.height,
+    )
+
+    # the PdfWriter to build the output file
+    writer = PdfWriter()
+
+    if groups is None:
+        # Fit systems on A4 page
+
+        height = page_layout.top_margin + page_layout.bottom_margin
+
+        current_group_size = 0
+        groups = []
+        for system in score.pages:
+            new_height = system.cropbox.height
+            new_height += page_layout.calculate_stave_height()
+
+            if height + new_height <= page_layout.minimum_height:
+                current_group_size += 1
+                height += new_height
+            elif height + new_height > page_layout.minimum_height and current_group_size == 0:
+                groups.append(1)
+                height = page_layout.top_margin + page_layout.bottom_margin
+            else:
+                groups.append(current_group_size)
+                height = page_layout.top_margin + page_layout.bottom_margin
+                current_group_size = 1
+
+    groups = groups_callback(score, groups)
+
+    # keep track of the current system (= a page in the input score)
+    current_system_number = 0
+
+    # Follow the groupings
+    for group in groups:
+        # get all the systems for this group
+        systems = [
+            score.pages[index]
+            for index in range(current_system_number, current_system_number + group)
+        ]
+
+        # increase the page number
+        current_system_number += group
+
+        # layout the systems
+        layout_systems(systems, staves, writer, page_layout)
+
+    # write to disk
+    print(output)
+    with open(output, "wb") as fp:
+        writer.write(fp)
